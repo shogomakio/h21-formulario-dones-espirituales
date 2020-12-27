11c14c899e594f467a88551d25569aa749ddba29
shogomakio <34526353+shogomakio@users.noreply.github.com>
2020-12-27 22:10:54 +0900

    Update functions.js
    estaba de ocioso y no estaba seguro si sabias de var, let y const, pero te dejo un pequenho code review
    cambie los var por const y let.

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,24 +1,45 @@
-var tiposDones = {
-    Don1: 'don1',
-    Don2: 'don2'
-}
+// var tiposDones = {
+//     Don1: 'don1',
+//     Don2: 'don2'
+// }
+// me parece que no lo estas usando en ninguna parte.
 
-var preguntasHtml = document.getElementById("preguntas");
+/*
+ya no se usa mucho var. En su lugar se usa const y let.
+const es constante, asi que su valor no cambia. Si no le vas a reasignar valor mejor usar const
+pero const tiene un comportamiento raro, si se le asigna un Object, se le puede cambiar el valor de las propiedades del Object
+let es igual que var solo que con alcance local (local scope). 
+var ya no se usa mucho porque su alcance es global y es dificil de seguir codigo y arreglar bugs si es que por error tenes dos variables con el mismo nombre.
+ejs:
+*/
+/*
+ej1:
+const perro = 'Mark'
+perro = 'Ramon' <- ERROR, no se le puede reasignar valor
+*/
+/*
+ej2:
+const perro = {name: 'Mark', race: 'chihuahua'}
+perro.name = 'Ramon' -> va a cambiar el valor a perro = {name : 'Ramon', race: 'chihuahua'}
+*/
+const preguntasHtml = document.getElementById("preguntas");
 function cargarPreguntas() {
-    var questionsHtml = '';
+    let questionsHtml = '';
 
-    var cont = 0;
-    for (var i = 1; i <= preguntas.length; i++) {
+    let cont = 0;
+    for (let i = 1; i <= preguntas.length; i++) {
 
-        var optionsHtml = '';
+        let optionsHtml = '';
         var pregunta = preguntas[cont];
-        for (var j = 0; j < pregunta.options.length; j++) {
-            var option = pregunta.options[j];
+        for (let = 0; j < pregunta.options.length; j++) {
+            const option = pregunta.options[j];
             optionsHtml += `<input type="radio" name="pregunta${i}" value="${option.val}" id="pregunta${i}-${j}" required data-tipo="${pregunta.type}"><label for="pregunta${i}-${j}">${option.desc}</label>`;
+            // es mi gusto pero preferiria que o todo este en spanish, o todo en ingles
         }
 
-        var pregunta = preguntas[cont];
-        var questionHtml = `<div class="pregunta"><label for="pregunta${i}">${pregunta.desc}</label><div class="opciones">${optionsHtml}</div></div>`;
+        // var pregunta = preguntas[cont];
+        // la linea de arriba es igual a la de la linea 31, creo que no es necesario.
+        const questionHtml = `<div class="pregunta"><label for="pregunta${i}">${pregunta.desc}</label><div class="opciones">${optionsHtml}</div></div>`;
 
         questionsHtml += questionHtml;
         cont++;        
